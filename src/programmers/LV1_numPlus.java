4b6583f64597a12af94850857bb2ef5c46634902
wonhoLee <lwonho@hotmail.com>
2018-08-15 12:31:32 +0900

    done

---
+++ src/programmers/LV1_numPlus.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class LV1_numPlus {
+
+	public static void main(String[] args) {
+		int N = 123;
+		//int N = 987;
+		System.out.println(solution(N));
+	}
+	
+	//다른사람의 풀이
+	public static int solution(int n) {
+        return String.valueOf(n).chars().map(i -> Integer.parseInt(String.valueOf((char)i))).sum();
+    }
+	
+	//case 1 : 나의 풀이
+	/*public static int solution(int n) {
+        return Arrays.stream(String.valueOf(n).split("")).mapToInt(i -> Integer.parseInt(i)).sum();
+    }*/
+
+}
