f5e1faa800c593b3c7053719412a4cb682ac6cea
wonhoLee <lwonho@hotmail.com>
2018-08-16 09:51:14 +0900

    done

---
+++ src/hackerrank/easy_leftRotation.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.util.Arrays;
+
+public class easy_leftRotation {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5};
+		int d = 4;
+		System.out.println(Arrays.toString(rotLeft(a, d)));
+	}
+
+	private static int[] rotLeft(int[] a, int d) {
+		for (int i = 0; i < d; i++) {
+			int nTemp = a[0];
+			for (int j = 0; j < a.length - 1; j++) {
+				a[j] = a[j + 1];
+			}
+			a[a.length - 1] = nTemp;
+		}
+		return a;
+    }
+}
