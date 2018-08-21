51a401f66d2dd50d207702b60b6f6e0aa9a81af4
wonhoLee <lwonho@hotmail.com>
2018-08-21 15:30:38 +0900

    done

--- src/hackerrank/medium_SherlockandAnagrams.java
+++ src/hackerrank/medium_SherlockandAnagrams.java
@@ -5,37 +5,30 @@ import java.util.Arrays;
 public class medium_SherlockandAnagrams {
 
 	public static void main(String[] args) {
-		//String s = "abba";		//4
+		String s = "abba";		//4
 		//String s = "abcd";		//0
 		//String s = "ifailuhkqq";		//3
 		//String s = "kkkk";		//10
-		String s = "cdcd";		//5
+		//String s = "cdcd";		//5
 		
 		System.out.println(sherlockAndAnagrams(s));
 	}
 	
 	private static int sherlockAndAnagrams(String s) {
 		int nRet = 0;
-		for (int i = 0; i < s.length(); i++) {
-			for (int j = i + 1; j <= s.length(); j++) {
+		
+		for (int i = 0; i < s.length(); i++) {//위치선정
+			for (int j = i + 1; j < s.length(); j++) {	//표본검출
 				String sTemp = s.substring(i, j);
-				String[] arTemp = sTemp.split("");
-				Arrays.sort(arTemp);
-				sTemp = Arrays.toString(arTemp);
 				
-				for (int k = i + 1; k <= s.length(); k++) {
+				for (int k = i + 1; k <= s.length(); k++) {	//비교대상검출
 					if((k + j - i) > s.length()) {
 						break;
 					}
 					String sTemp2 = s.substring(k, k + j - i);
 					
-					String[] arTemp2 = sTemp2.split("");
-					Arrays.sort(arTemp2);
-					sTemp2 = Arrays.toString(arTemp2);
-					
-					if(!sTemp.equals("[]") && sTemp.equals(sTemp2)) {
+					if(check_anagram(sTemp, sTemp2)==1)
 						nRet++;
-					}
 				}
 			}
 		}
@@ -43,18 +36,66 @@ public class medium_SherlockandAnagrams {
 		return nRet;
     }
 	
+	//RESULT : 참조
+	/*private static int sherlockAndAnagrams(String s) {
+		int nRet = 0;
+		char[] sa;
+        sa = s.toCharArray();
+        
+		for(int index=1; index < sa.length; index++){
+            for(int i=0;i<sa.length-index+1;i++){
+                String s1 = s.substring(i,index+i);
+               for(int j=i+1;j<sa.length-index+1;j++){
+                   String s2 = s.substring(j,index+j);
+                   if(check_anagram(s1,s2)==1)
+                       nRet++;
+                }
+            }
+        }
+
+		return nRet;
+    }*/
+	
+	public static int check_anagram(String s1, String s2)
+    {
+        char a[] = s1.toCharArray();
+        char b[] = s2.toCharArray();
+        //System.out.println(s1+" "+s2);
+       int first[] = new int[26];
+        int second[] = new int[26];
+        Arrays.fill(first,0);
+        Arrays.fill(second,0);
+        int c=0;
+       while (c<a.length) {
+          first[a[c]-'a']++;
+          c++;
+       }
+       c = 0;
+       while (c<b.length) {
+          second[b[c]-'a']++;
+          c++;
+       }
+
+       for (c = 0; c < 26; c++) {
+          if (first[c] != second[c])
+             return 0;
+       }
+
+       return 1;
+    }
+	
 	//CASE 2: 타임오버
 	/*private static int sherlockAndAnagrams(String s) {
 		int nRet = 0;
 		
-		for (int i = 0; i < s.length(); i++) {
-			for (int j = i + 1; j <= s.length(); j++) {
+		for (int i = 0; i < s.length(); i++) {//위치선정
+			for (int j = i + 1; j < s.length(); j++) {	//표본검출
 				String sTemp = s.substring(i, j);
 				String[] arTemp = sTemp.split("");
 				Arrays.sort(arTemp);
 				sTemp = Arrays.toString(arTemp);
 				
-				for (int k = i + 1; k <= s.length(); k++) {
+				for (int k = i + 1; k <= s.length(); k++) {	//비교대상검출
 					if((k + j - i) > s.length()) {
 						break;
 					}
