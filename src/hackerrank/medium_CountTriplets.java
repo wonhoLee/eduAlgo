422111659f6c0fb30d559be22790ffc1c3c3a675
wonhoLee <lwonho@hotmail.com>
2018-08-22 16:40:48 +0900

    풀이중

---
+++ src/hackerrank/medium_CountTriplets.java
@@ -0,0 +1,26 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class medium_CountTriplets {
+
+	public static void main(String[] args) {
+		List<Long> arr = new ArrayList<>();
+		arr.add((long) 1);
+		arr.add((long) 4);
+		arr.add((long) 16);
+		arr.add((long) 64);
+		long r = 4;
+		
+		System.out.println(countTriplets(arr, r));
+	}
+
+	private static long countTriplets(List<Long> arr, long r) {
+		long nRet = 0;
+		
+		
+		
+		return nRet;
+    }
+}
