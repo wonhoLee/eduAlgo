82beed134f5edc6f6f3b546c3dd266e6727eafbf
wonho <lwonho@hotmail.com>
2018-10-05 18:45:20 +0900

    modify

--- src/programmers/hash/LV1_mara.java
+++ src/programmers/hash/LV1_mara.java
@@ -19,12 +19,12 @@ public class LV1_mara {
 		String answer = "";
 		Map<String, Integer> map = new HashMap<>();
 		
-		for (int i = 0; i < participant.length; i++) {
-			map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+		for (String str : participant) {
+			map.put(str, map.getOrDefault(str, 0) + 1);
 		}
 		
-		for (int i = 0; i < completion.length; i++) {
-			map.put(completion[i], map.get(completion[i]) - 1);
+		for (String str : completion) {
+			map.put(str, map.get(str) - 1);
 		}
 		
 		for (String key : map.keySet()) {
