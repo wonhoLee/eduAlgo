39a7842e7fa3102f37f0678112fa7db2dbb5f672
wonho <lwonho@hotmail.com>
2018-10-25 17:51:37 +0900

    Q559풀이

---
+++ src/main/java/me/algo/leetcode/Q559_MaximumDepthOfNaryTree.java
@@ -0,0 +1,42 @@
+package me.algo.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Q559_MaximumDepthOfNaryTree {
+	static class Node {
+	    public int val;
+	    public List<Node> children;
+
+	    public Node() {}
+
+	    public Node(int _val,List<Node> _children) {
+	        val = _val;
+	        children = _children;
+	    }
+	};
+	
+	public int maxDepth(Node root) {
+        int ret = 0;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+        	ret++;
+        	int size = q.size();
+        	for (int i = 0; i < size; i++) {
+        		Node node = q.poll();
+				for(Node child : node.children) {
+					q.offer(child);
+				}
+			}
+        }
+        
+        return ret;
+    }
+
+	public static void main(String[] args) {
+		
+	}
+}
