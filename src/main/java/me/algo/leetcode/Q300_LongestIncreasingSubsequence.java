443149d30e5863f331a08507d659dde8daec6b39
wonhoLee <lwonho@hotmail.com>
2018-11-08 20:31:42 +0900

    문제풀이

--- src/main/java/me/algo/leetcode/Q300_LongestIncreasingSubsequence.java
+++ src/main/java/me/algo/leetcode/Q300_LongestIncreasingSubsequence.java
@@ -1,7 +1,7 @@
 package me.algo.leetcode;
 
 public class Q300_LongestIncreasingSubsequence {
-	public int lengthOfLIS(int[] nums) {
+	public static int lengthOfLIS(int[] nums) {
 		if(nums == null || nums.length == 0) return 0;
 		//점화식
 		//d[i] = 나를 포함하는 가장 긴 LIS의 길이
@@ -26,6 +26,7 @@ public class Q300_LongestIncreasingSubsequence {
     }
 
 	public static void main(String[] args) {
-		
+		int[] nums = {10,9,2,5,3,7,101,18};
+		System.out.println(lengthOfLIS(nums));
 	}
 }
