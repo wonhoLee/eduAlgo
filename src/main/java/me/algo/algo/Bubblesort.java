e563cca64eb4c003b1624c7e85a5a279c5a23a89
wonho <lwonho@hotmail.com>
2020-03-22 15:14:48 +0900

    소스정리

--- src/main/java/me/algo/algo/Bubblesort.java
+++ src/main/java/me/algo/algo/Bubblesort.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 public class Bubblesort {
 
 	public static void main(String[] args) {
-		int[] A = {9, 5, 7, 3, 2, 7, 3, 1, 10, 8};
+		int[] A = { 9, 5, 7, 3, 2, 7, 3, 1, 10, 8 };
 		bubbleSort(A);
 	}
 
 	private static void bubbleSort(int[] arr) {
-	    int temp = 0;
-		for(int i = 0; i < arr.length; i++) {
-			for(int j= 1 ; j < arr.length-i; j++) {
-				if(arr[j]<arr[j-1]) {
-					temp = arr[j-1];
-					arr[j-1] = arr[j];
+		int temp = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 1; j < arr.length - i; j++) {
+				if (arr[j] < arr[j - 1]) {
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
 					arr[j] = temp;
 				}
 			}
--- src/main/java/me/algo/algo/Gcdlcm.java
+++ src/main/java/me/algo/algo/Gcdlcm.java
@@ -7,33 +7,28 @@ public class Gcdlcm {
 		System.out.println(gcd3(24, 36));
 	}
 
-	public static int gcd(int a,int b)
-	{ //�ݺ����� �̿��� ���
-	    int mod = a % b;
-	    while(mod > 0)
-	    {
-	        a = b;
-	        b = mod;
-	        mod = a % b;
-	    }
-	    return b;
+	public static int gcd(int a, int b) {
+		int mod = a % b;
+		while (mod > 0) {
+			a = b;
+			b = mod;
+			mod = a % b;
+		}
+		return b;
 	}
-	 
-	public static int gcd2(int a,int b)
-	{ //��� �Լ���
-	    if( a % b == 0 )
-	        return b;
-	    else
-	        return gcd2(b, a % b);
+
+	public static int gcd2(int a, int b) {
+		if (a % b == 0)
+			return b;
+		else
+			return gcd2(b, a % b);
 	}
-	 
-	public static int gcd3(int a, int b)
-	{ //���� ������ ����� 
-	    return (a % b == 0 ? b : gcd3(b,a%b));
+
+	public static int gcd3(int a, int b) {
+		return (a % b == 0 ? b : gcd3(b, a % b));
 	}
-	
+
 	public static int lcm(int a, int b) {
-		return a * b / gcd3(a,b);
+		return a * b / gcd3(a, b);
 	}
 }
-
