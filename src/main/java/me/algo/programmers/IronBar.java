4b845cd009bf5642194ed21006f52855364feb25
wonho <lwonho@hotmail.com>
2020-04-11 11:27:44 +0900

    study

--- src/main/java/me/algo/programmers/IronBar.java
+++ src/main/java/me/algo/programmers/IronBar.java
@@ -1,5 +1,7 @@
 package me.algo.programmers;
 
+import java.util.Stack;
+
 public class IronBar {
     public static void main(String[] args) {
         String arrangement = "()(((()())(())()))(())";
@@ -7,11 +9,42 @@ public class IronBar {
         System.out.println(ironBar.solution(arrangement));
     }
 
-    public int solution(String arrangement) {
+    public static int solution(String arrangement) {
         int answer = 0;
-
-
-
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arrangement.length(); i++) {
+            if (arrangement.charAt(i) == '(') {
+                st.push(i);
+            } else if (arrangement.charAt(i) == ')') {
+                if (st.peek() + 1 == i) {
+                    st.pop();
+                    answer += st.size();
+                } else {
+                    st.pop();
+                    answer += 1;
+                }
+            }
+        }
         return answer;
     }
+
+//    public int solution(String arrangement) {
+//        int answer = 0;
+//
+//        String[] arrangeArr = arrangement.replace("()", "0").split("");
+//        Stack<String> stack = new Stack<>();
+//
+//        for (String arr : arrangeArr) {
+//            if (arr.equals("(")) {
+//                stack.push(arr);
+//            } else if (arr.equals(")")) {
+//                stack.pop();
+//                answer++;
+//            } else if (arr.equals("0")) {
+//                answer += stack.size();
+//            }
+//        }
+//
+//        return answer;
+//    }
 }
