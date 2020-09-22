7146dd9eab7cdc03fb5826bc1129a41f9f8cfa36
wonho <lwonho@hotmail.com>
2020-09-22 22:17:13 +0900

    study

--- src/main/java/me/algo/algo/sort/MergeSort.java
+++ src/main/java/me/algo/algo/sort/MergeSort.java
@@ -6,54 +6,83 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] A = {9, 5, 7, 3, 2, 7, 3, 1, 10, 8};
-		int[] sorted = new int[A.length];
+		int[] array = {9, 5, 7, 3, 2, 7, 3, 1, 10, 8};
+		int[] sorted = new int[array.length];
 //		mergeSort(A, 0, A.length - 1, sorted);
-		wonSort(A, 0, A.length - 1, sorted);
+		wonSort(array, 0, array.length - 1, sorted);
 		System.out.println(Arrays.toString(sorted));
 	}
 
-	private static void mergeSort(int[] a, int m, int n, int[] sorted) {
-		if (m < n) {
-			int middle = (m + n) / 2;
-			mergeSort(a, m, middle, sorted);
-			mergeSort(a, middle + 1, n, sorted);
-			merge(a, m, middle, n, sorted);
+	private static void mergeSort(int[] array, int low, int high, int[] sorted) {
+		if (low < high) {
+			int middle = (low + high) / 2;
+			mergeSort(array, low, middle, sorted);
+			mergeSort(array, middle + 1, high, sorted);
+			merge(array, sorted, low, middle, high);
 		}
 	}
 
-	private static void merge(int[] a, int m, int middle, int n, int[] sorted) {
-		int i = m;
-		int j = middle + 1;
-		int k = m;
-
-		while (i <= middle && j <= n) {
-			if (a[i] <= a[j]) {
-				sorted[k] = a[i];
-				i++;
-			} else {
-				sorted[k] = a[j];
-				j++;
-			}
-			k++;
+	private static void merge(int[] array, int[] helper, int low, int middle, int high) {
+		// 절반짜리 두 배열을 helper 배열에 복사한다.
+		for (int i = low; i <= high; i++) {
+			helper[i] = array[i];
 		}
 
-		if (i > middle) {
-			for (int k2 = j; k2 <= n; k2++) {
-				sorted[k] = a[k2];
-				k++;
+		int helperLeft = low;
+		int helperRight = middle + 1;
+		int current = low;
+
+		//helper 배열 순회, 왼쪽 절반과 오른쪽 절반을 비교하여 작은 원소를 원래 배열에 복사해 넣는다.
+		while (helperLeft <= middle && helperRight <= high) {
+			if (helper[helperLeft] <= helper[helperRight]) {
+				array[current] = helper[helperLeft];
+				helperLeft++;
+			} else {    //오른쪽 원소가 왼쪽 원소보다 작으면
+				array[current] = helper[helperRight];
+				helperRight++;
 			}
-		} else {
-			for (int k2 = i; k2 <= middle; k2++) {
-				sorted[k] = a[k2];
-				k++;
-			}
+			current++;
 		}
 
-		for (int k2 = m; k2 <= n; k2++) {
-			a[k2] = sorted[k2];
+		// 왼쪽 절반 배열에 남은 원소들을 원래 배열에 복사해 넣는다.
+		int remaining = middle - helperLeft;
+		for (int i = 0; i <= remaining; i++) {
+			array[current + i] = helper[helperLeft + i];
 		}
 	}
+
+//	private static void merge(int[] array, int low, int middle, int high, int[] sorted) {
+//		int helperLeft = low;
+//		int helperRight = middle + 1;
+//		int current = low;
+//
+//		while (helperLeft <= middle && helperRight <= high) {
+//			if (array[helperLeft] <= array[helperRight]) {
+//				sorted[current] = array[helperLeft];
+//				helperLeft++;
+//			} else {
+//				sorted[current] = array[helperRight];
+//				helperRight++;
+//			}
+//			current++;
+//		}
+//
+//		if (helperLeft > middle) {
+//			for (int k2 = helperRight; k2 <= high; k2++) {
+//				sorted[current] = array[k2];
+//				current++;
+//			}
+//		} else {
+//			for (int k2 = helperLeft; k2 <= middle; k2++) {
+//				sorted[current] = array[k2];
+//				current++;
+//			}
+//		}
+//
+//		for (int k2 = low; k2 <= high; k2++) {
+//			array[k2] = sorted[k2];
+//		}
+//	}
 
 	private static void wonSort(int[] source, int start, int end, int[] target) {
 		if (start < end) {
--- src/main/java/me/algo/programmers/kakao2018/K3_NGame.java
+++ src/main/java/me/algo/programmers/kakao2018/K3_NGame.java
@@ -1,4 +1,52 @@
 package me.algo.programmers.kakao2018;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class K3_NGame {
+    public static void main(String[] args) {
+        int n = 2;
+        int t = 4;
+        int m = 2;
+        int p = 1;
+        K3_NGame k3_nGame = new K3_NGame();
+        System.out.println(k3_nGame.solution(n, t, m, p));
+    }
+
+    /*
+    0부터 숫자 올리기
+    n진수 변환
+
+     */
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < t * m; i++) {
+            int value = i;
+            while (value != 0) {
+                if ((value % n) < 10) {
+                    answer.insert(0, (value % n));
+                    value /= n;
+                } else {
+                    int temp = (char) ((value % n) + 55);
+                    answer.insert(0, temp);
+                }
+            }
+
+            if (answer.toString().equals("")) {
+                answer.append("0");
+            }
+            Collections.addAll(list, answer.toString().split(""));
+            answer.delete(0, answer.length());
+        }
+
+        for (int i = p - 1; i < list.size(); i += (p + 1)) {
+            result.append(list.get(i));
+            if (result.length() == t) break;
+        }
+
+        return result.toString();
+    }
 }
